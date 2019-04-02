957914b7b7546ba93098b09230d6a348822efe4a
Vipul Singh <singh061@rangers.uwp.edu>
2019-04-02 02:06:46 -0500

    in the process of making the min screen

---
+++ AddingGame/InitialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitialViewController.swift
+//  AddingGame
+//
+//  Created by Vipul Singh on 4/2/19.
+//  Copyright © 2019 Vipul Singh. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
