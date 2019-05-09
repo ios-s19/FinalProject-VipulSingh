b88cdb453d8f83ea0e5ee4d58eaa12ededd48cb0
Vipul Singh <singh061@rangers.uwp.edu>
2019-05-09 03:23:29 -0500

    generating number and input at this point

--- AddingGame/InitialViewController.swift
+++ AddingGame/InitialViewController.swift
@@ -9,26 +9,96 @@
 import UIKit
 
 class InitialViewController: UIViewController {
+    
+    //create outlets
+    //score
+    @IBOutlet weak var scoreLabel:UILabel?
+    //input text field
+    @IBOutlet weak var inputField:UITextField?
+    //number display label
+    @IBOutlet weak var numbersDisplayLabel:UILabel?
+    
+    //create score property
+    var score : Int = 0
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
+        //starting point for our game
+        //set rand number label
+        // update score label
+        
+        setRandomNumberLabel()
+        updateScore()
+        
+        //when somebody types something in, we want to respond back to them
+        inputField?.addTarget(self, action: #selector(textFieldChanged(textField:)), for:UIControl.Event.editingChanged)
+        
+        
         // Do any additional setup after loading the view.
     }
+    //method for updating the socres label
+    func updateScore()
+    {
+        
+        scoreLabel?.text = "\(score)"
+    }
     
-    override func didReceiveMemoryWarning() {
+    func setRandomNumberLabel()
+    {
+        numbersDisplayLabel?.text = generateRandomNumbers()
+    }
+    @objc func textFieldChanged(textField:UITextField)
+    {
+        //want to make sure text is 5 characters
+        if inputField?.text?.characters.count ?? 0 < 4
+        {return}
+        if  let numbers_text    = numbersDisplayLabel?.text,
+            let input_text      = inputField?.text,
+            let numbers = Int(numbers_text),
+            let input   = Int(input_text)
+        {
+            //print input text
+            print("Comparing: \(input_text) minus \(numbers_text) == \(input - numbers)")
+            
+            
+            //increment or decrement score based on user input
+            if(input - numbers == 1111)
+            {
+                print("Correct!")
+                score = score + 1
+            }
+            else
+            {
+                print("Incorrect!")
+                score = score - 1
+            }
+        }
+        setRandomNumberLabel()
+        updateScore()
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
     }
     
+    
+    
     //generate random numbers
-    func generateRandomNumber() -> String
+    func generateRandomNumbers() -> String
     {
-        var result:String = ""
+        //empty string
+        var result : String = ""
         
+        //loop will repoeat 4 times
         for _ in 1...4
         {
-            let digit = Int.random(in: 1..<9)
+            //return random value including nine
+            var digit:Int=Int(arc4random_uniform(8) + 1)
             
+            
+            //append string value "digit" to result
             result += "\(digit)"
         }
         
